77548f9029fb9e766d0061f71af0d5a91a0e7f35
Gian Hiltbrunner <gian.silvan.hiltbrunner@gmail.com>
2022-05-24 23:37:21 +0200

    Update zurigrad.rb

--- Casks/zurigrad.rb
+++ Casks/zurigrad.rb
@@ -1,6 +1,6 @@
 cask "zurigrad" do
   version "2.0"
-  sha256 "973a83e1049818d77ed581465aa2d51990f924ddb67a4304a979ddea8106732e"
+  sha256 "c642f2748b2802c1af7f11596ae6338784697f870646dcf7de330477df218272"
 
   url "https://github.com/gianhiltbrunner/Zurigrad/releases/download/b#{version}/Zurigrad.app.zip"
   name "Zurigrad"
